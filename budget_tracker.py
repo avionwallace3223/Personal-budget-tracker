474776a013dcf8547090619ffe937e418ec412c4
avionwallace3223 <avionwallace8@gmail.com>
2026-09-11 00:38:12 -0500

    Add Expense Categories

--- budget_tracker.py
+++ budget_tracker.py
@@ -2,7 +2,13 @@ def calculate_remaining(income, expenses):
   return income - expenses
 
 income = float(input("2000: $"))
-expenses = float(input(" 1000: $"))
+
+rent = float(input("417: $"))
+transportation = float(input("200: $"))
+food = float(input("200: $"))
+entertainment = float(input("200: $"))
+
+expenses = rent + transportation + food + entertainment
 
 remaining = calculate_remaining(income, expenses):
 print("money remaining:", remaining)
