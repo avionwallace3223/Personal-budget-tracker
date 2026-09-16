1072e3f834621c4d36bec7407bf76d8ac93e9965
avionwallace3223 <avionwallace8@gmail.com>
2026-09-16 01:07:04 -0500

    Added largest Expense tracker

--- budget_tracker.py
+++ budget_tracker.py
@@ -52,14 +52,17 @@ total_expenses = rent + transportation + food + entertainment + custom_expenses
 remaining = calculate_remaining(income, total_expenses)
 savings_percentage = (remaining / income) * 100
   
+all_expenses = list(zip(expenses, amounts))
+largest_expense = max(all_expenses, key=lambda expense: expense[1])
+
 print("\n===== MONTHLY BUDGET =====")
 print(f"Income: ${income:.2f}")
 print(f"Rent: ${rent:.2f}")
 print(f"Transportation: ${transportation:.2f}")
 print(f"Food: ${food:.2f}")
 print(f"Entertainment: ${entertainment:.2f}")
-
-print("\nCustome Expenses:")
+print("\nCustom Expenses:")
+print(f"Largest Custom Expense: {largest_expense[0]} ${largest_expense[1]:.2f}")
 
 for i in range(len(expenses)):
   print(f"{expenses[i]}: ${amounts[i]:.2f}" )
